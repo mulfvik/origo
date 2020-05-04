58cf7f0653b3c6f078cfb5119826982e72c73d53
Mikael Ulfvik <ulfvik.mikael@outlook.com>
2020-05-04 09:21:27 +0200

    Setup

---
+++ src/controls/translation/model.js
@@ -0,0 +1,10 @@
+
+class TranslationModel {
+  constructor(translationRespons) {
+    this.maxLengthText = translationRespons.controls.editor.editform.maxLengthText
+    // console.log(this.maxLengthText);
+  }
+}
+export {
+  TranslationModel
+}
