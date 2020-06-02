ace4413573bdfd38c0cd99a9e632fe039ff2babb
Mikael Ulfvik <ulfvik.mikael@outlook.com>
2020-06-02 23:06:46 +0200

    Adds Cesium Ion 3D-tiles options

--- src/controls/globe/globe.js
+++ src/controls/globe/globe.js
@@ -3,26 +3,25 @@ import OLCesium from 'olcs/OLCesium';
 import { Component, Button, dom } from '../../ui';
 
 const Globe = function Globe(options = {}) {
-  // CI... is short for Cesium Ion...
   let {
     target
   } = options;
-
+  // CI... is short for Cesium Ion...
   const {
     CIToken,
     CIAssetIdTerrain,
-    CIAssetId3DTiles
+    CIAssetId3DTiles = 96188
   } = options;
 
   let map;
   let viewer;
-  let globeButton;
   let ol3d;
+  let globeButton;
 
   // To use Cesium Ion features token needs to be provided in config option CIToken
   Cesium.Ion.defaultAccessToken = CIToken;
 
-  // Toggles between 2D and 3D with the globeButton
+  // Toggles between 2D and 3D
   const toggleGlobe = () => {
     ol3d.setEnabled(!ol3d.getEnabled());
   };
@@ -37,8 +36,9 @@ const Globe = function Globe(options = {}) {
     });
   };
 
+  // Terrain
   const terrainProviders = (scene) => {
-    // If asset id is provided for terrain that terrain is used, else a world terrain is used as default
+    // If asset id is provided that terrain is used, else a world terrain is used as default
     if (CIAssetIdTerrain) {
       const CITerrainProvider = new Cesium.CesiumTerrainProvider({
         requestVertexNormals: true,
@@ -53,6 +53,17 @@ const Globe = function Globe(options = {}) {
     }
   };
 
+  // 3D-tiles
+  const tilesProviders = (scene) => {
+    // If asset id is provided that 3d tiles is used, else OSM 3D-tileset is used
+    if (CIAssetId3DTiles) {
+      const CITileset = new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(CIAssetId3DTiles)
+      });
+      scene.primitives.add(CITileset);
+    }
+  };
+
   return Component({
     name: 'globe',
     onAdd(evt) {
@@ -61,9 +72,10 @@ const Globe = function Globe(options = {}) {
       ol3d = new OLCesium({ map });
       const scene = ol3d.getCesiumScene();
       noCameraBelowTerrain(scene);
-      // If token is provided there is access to Cesium Ion and assets
+      // If token is provided there is access to Cesium Ion and your assets, else define own endpoints for providers
       if (CIToken) {
         terrainProviders(scene);
+        tilesProviders(scene);
       }
       if (!target) target = `${viewer.getMain().getNavigation().getId()}`;
       this.on('render', this.onRender);
